944d60e9ae5831f955ae96d9abbde5b14679b2ff
Scott Parry <hello@ikreativ.com>
2012-04-16 15:29:21 +0100

    a ton of improvements

--- assets/js/application.js
+++ assets/js/application.js
@@ -24,26 +24,6 @@ $("nav select").change(function() {
   window.location = $(this).find("option:selected").val();
 });
 
-// Fix subnav on scroll
-var $win = $(window), 
-$nav = $('.subnav'), 
-navTop = $('.subnav').length && $('.subnav').offset().top - 40, 
-isFixed = 0
-
-processScroll()
-$win.on('scroll', processScroll)
-
-function processScroll() {
-	var i, scrollTop = $win.scrollTop()
-    if (scrollTop >= navTop && !isFixed) {
-    	isFixed = 1
-        $nav.addClass('subnav-fixed')
-    } else if (scrollTop <= navTop && isFixed) {
-        isFixed = 0
-        $nav.removeClass('subnav-fixed')
-    }
-}
-
 // Pretty Photo
 $("a[class^='prettyPhoto']").prettyPhoto();
 
