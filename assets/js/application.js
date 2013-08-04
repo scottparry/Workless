9f183fe765ce3aebd948acf8542baa0b8104a8d8
Scott Parry <hello@ikreativ.com>
2013-08-04 19:02:14 +0100

    Remove tipsy and prettyphoto

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,32 +1,7 @@
 jQuery(document).ready(function($) {
 
-	// Pretty Photo
-	$("a[class^='prettyPhoto']").prettyPhoto();
-
-	// Tipsy
-	$('.tooltip').tipsy({
-		gravity: $.fn.tipsy.autoNS,
-		fade: true,
-		html: true
-	});
-
-	$('.tooltip-s').tipsy({
-		gravity: 's',
-		fade: true,
-		html: true
-	});
-
-	$('.tooltip-e').tipsy({
-		gravity: 'e',
-		fade: true,
-		html: true
-	});
-
-	$('.tooltip-w').tipsy({
-		gravity: 'w',
-		fade: true,
-		html: true
-	});
+	// Responsive menu
+  	$(".open").pageslide();
 
 	// Prettyprint
 	$('pre').addClass('prettyprint');
