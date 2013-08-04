64b5ba2a0d75339b242110412505334ee455d9fa
Scott Parry <hello@ikreativ.com>
2013-08-04 12:25:38 +0100

    Removed select menu for responsive
    We'll be changing this to a better solution

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,64 +1,40 @@
 jQuery(document).ready(function($) {
-	
-// Select nav for smaller resolutions
-// Select menu for smaller screens
-$("<select />").appendTo("nav#primary");
 
-// Create default option "Menu"
-$("<option />", {
-   "selected": "selected",
-   "value"   : "",
-   "text"    : "Menu"
-}).appendTo("nav#primary select");
+	// Pretty Photo
+	$("a[class^='prettyPhoto']").prettyPhoto();
 
-// Populate dropdown with menu items
-$("nav#primary a").each(function() {
- var el = $(this);
- $("<option />", {
-     "value"   : el.attr("href"),
-     "text"    : el.text()
- }).appendTo("nav select");
-});
+	// Tipsy
+	$('.tooltip').tipsy({
+		gravity: $.fn.tipsy.autoNS,
+		fade: true,
+		html: true
+	});
 
-$("nav#primary select").change(function() {
-  window.location = $(this).find("option:selected").val();
-});
+	$('.tooltip-s').tipsy({
+		gravity: 's',
+		fade: true,
+		html: true
+	});
 
-// Pretty Photo
-$("a[class^='prettyPhoto']").prettyPhoto();
+	$('.tooltip-e').tipsy({
+		gravity: 'e',
+		fade: true,
+		html: true
+	});
 
-// Tipsy
-$('.tooltip').tipsy({
-	gravity: $.fn.tipsy.autoNS,
-	fade: true,
-	html: true
-});
+	$('.tooltip-w').tipsy({
+		gravity: 'w',
+		fade: true,
+		html: true
+	});
 
-$('.tooltip-s').tipsy({
-	gravity: 's',
-	fade: true,
-	html: true
-});
+	// Scroll
+	jQuery.localScroll();
 
-$('.tooltip-e').tipsy({
-	gravity: 'e',
-	fade: true,
-	html: true
-});
+	// Prettyprint
+	$('pre').addClass('prettyprint linenums');
 
-$('.tooltip-w').tipsy({
-	gravity: 'w',
-	fade: true,
-	html: true
-});
-
-// Scroll
-jQuery.localScroll();
-
-// Prettyprint
-$('pre').addClass('prettyprint linenums');
-
-// Uniform
-$("select, input:checkbox, input:radio, input:file").uniform();
+	// Uniform
+	$("select, input:checkbox, input:radio, input:file").uniform();
 	
 });
