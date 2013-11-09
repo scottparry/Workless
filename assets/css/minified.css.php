a4b20f98be9b7d839c1293ebc4d44960626f5ca9
Scott Parry <hello@ikreativ.com>
2013-11-09 20:34:02 +0000

    Added animations.css
    CSS3 animations

--- assets/css/minified.css.php
+++ assets/css/minified.css.php
@@ -1,34 +1,35 @@
-<?php 
+<?php
 header('Content-type: text/css');
 ob_start("compress");
 
-  	function compress($buffer) {
-  		  /* remove comments */
-      	$buffer = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $buffer);
-      		
-      	/* remove tabs, spaces, newlines, etc. */
-      	$buffer = str_replace(array("\r\n", "\r", "\n", "\t", '  ', '    ', '    '), '', $buffer);
-      		
-      	return $buffer;
-  	}
+  function compress($buffer) {
+    /* remove comments */
+      $buffer = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $buffer);
 
-  	/* css files for compression */
-  	include('plugins.css');
-  	include('workless.css');
-  	include('typography.css');
-    include('font.css');
-  	include('forms.css');
-  	include('tables.css');
-  	include('buttons.css');
-  	include('alerts.css');
-  	include('tabs.css');
-  	include('pagination.css');
-  	include('breadcrumbs.css');
+      /* remove tabs, spaces, newlines, etc. */
+      $buffer = str_replace(array("\r\n", "\r", "\n", "\t", '  ', '    ', '    '), '', $buffer);
+
+      return $buffer;
+  }
+
+    /* css files for compression */
+    include('plugins.css');
+    include('workless.css');
+    include('typography.css');
+    include('forms.css');
+    include('tables.css');
+    include('buttons.css');
     include('backgrounds.css');
-  	include('helpers.css');
+    include('alerts.css');
+    include('pagination.css');
+    include('breadcrumbs.css');
+    include('font.css');
+    include('helpers.css');
     include('scaffolding.css');
-  	include('application.css');
-    include('responsive.css');
     include('print.css');
+    include('animation.css');
+    include('application.css');
+    include('responsive.css');
 
 ob_end_flush();
+?>
